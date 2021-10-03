c270557d996380c73fcb17a704788c94027ad249
Rakshya Sharma <73920115+rsharma62@users.noreply.github.com>
2021-10-03 00:37:58 -0700

    Python

---
+++ astr-119-hw-2/hw-2-outputs/while.py
@@ -0,0 +1,8 @@
+# a while loop acts on a conditional
+# While that condition is True the program exectues and stops when false
+
+i = 0	#defines an integer
+
+while (i<119):	#while is less than 119, do the folowing
+	print(i) 	#print the current value of i
+	i += 10 	#add 10 to 1
