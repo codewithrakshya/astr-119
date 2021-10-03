e007f748bb8ab0a644f72a95f219ec2aa7e44c69
Rakshya Sharma <73920115+rsharma62@users.noreply.github.com>
2021-10-02 23:00:34 -0700

    Python : Operators

---
+++ astr-119-hw-2/operators.py
@@ -0,0 +1,42 @@
+x = 9
+y = 3
+
+# Arithematic Operators
+print(x+y)	# Addition
+print(x-y)  # Substraction
+print(x*y)	# Multiplication
+print(x/y)	# Division
+print(x%y)	# Modulus
+print(x**y)	# Exponentiation
+x = 9.191823
+print("Floor divide: ", x//y)
+print("Modulo: ", x%y)
+
+#assignment operators
+x = 9 	# set x = 9
+x += 3 	# x = x + 3
+print(x) 
+
+x = 9
+x -= 3 	# x = x - 3
+print(x)
+
+x *= 3 	# x = x * 3
+print(x)
+
+x /= 3 	# x = x / 3
+print(x)
+
+x **= 3 	# x = x ** 3
+print(x)
+
+# Comparison Operator
+
+x = 9
+y = 3
+print("Does x==y? ", x==y)
+print("Does x!=y? ", x!=y)
+print("Is x>y? ", x>y)
+print("Is x<y? ", x<y)
+print("Is x>=y? ", x>=y)
+print("Is x<=y? ", x<=y)
