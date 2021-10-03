c25fd3b2338a53ba041d612d1968b3eae59b7f43
Rakshya Sharma <73920115+rsharma62@users.noreply.github.com>
2021-10-03 00:58:36 -0700

    Python: Useful Modules

---
+++ astr-119-hw-2/useful_modules.py
@@ -0,0 +1,11 @@
+# followed Brant's example from slide 25 of Session 3 pdf
+
+import numpy as np #the Numpy library 
+import matplotlib.pyplot as plt		#Matplotlib's pyplot
+
+import sys		#gives access to a C-like sys library
+import os		#gives access to operating system  
+
+print(sys.argv)	#prints any command line arguments, incl program name
+print(os.getcwd())	#prints the current working directory
+
