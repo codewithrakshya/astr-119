e304012ca8a3653ba4a6f03cfe871e646e732f81
Rakshya Sharma <73920115+rsharma62@users.noreply.github.com>
2021-10-05 11:28:56 -0700

    Session 4

---
+++ Session 4/python_useful_modules.py
@@ -0,0 +1,11 @@
+# followed examples from Brant's examples on session 4
+
+import numpy as np 					#the Numpy library
+import matplotlib.pyplot as plt		#Matplotlib's pyplot
+
+import sys							#gives access to a C-like sys library
+import os							#gives access to operating system
+
+print(sys.argv)						#prints any commans line arguments, incl program name
+print(os.getcwd())					#prints the current working directory
+	
